curator/okta-aws-cli

commit 9f8a7ecf1d12420c5d212f0df40a1f861fbeff21

Logger interface to control stdio printing depending on which format okta-aws-cli is outputting to.
Closes #247

internal/webssoauth/webssoauth.go
@@ -135,7 +135,7 @@ func NewWebSSOAuthentication(cfg *config.Config) (token *WebSSOAuthentication, e
 
 	// Check if exec arg is present and that there are args for it before doing any work
 	if cfg.Exec() {
-		if _, err := exec.NewExec(); err != nil {
+		if _, err := exec.NewExec(cfg); err != nil {
 			return nil, err
 		}
 	}
@@ -275,7 +275,7 @@ func (w *WebSSOAuthentication) selectFedApp(apps []*okta.Application) (string, e
 				if err != nil {
 					return "", err
 				}
-				fmt.Fprintln(os.Stderr, rich)
+				w.config.Logger.Warn(rich)
 			}
 
 			return app.ID, nil
@@ -334,7 +334,7 @@ func (w *WebSSOAuthentication) establishTokenWithFedAppID(clientID, fedAppID str
 		}
 
 		if w.config.Exec() {
-			exe, _ := exec.NewExec()
+			exe, _ := exec.NewExec(w.config)
 			if err := exe.Run(cc); err != nil {
 				return err
 			}
@@ -344,7 +344,7 @@ func (w *WebSSOAuthentication) establishTokenWithFedAppID(clientID, fedAppID str
 		for cc := range ccch {
 			err = output.RenderAWSCredential(w.config, cc)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to render credential %s: %s\n", cc.Profile, err)
+				w.config.Logger.Warn("failed to render credential %s: %s\n", cc.Profile, err)
 				continue
 			}
 		}
@@ -406,7 +406,7 @@ func (w *WebSSOAuthentication) awsAssumeRoleWithSAML(iar *idpAndRole, assertion,
 	})
 	if p, err := w.fetchAWSAccountAlias(sessCopy); err != nil {
 		org := "org"
-		fmt.Fprintf(os.Stderr, "unable to determine account alias, setting alias name to %q\n", org)
+		w.config.Logger.Warn("unable to determine account alias, setting alias name to %q\n", org)
 		profileName = org
 	} else {
 		profileName = p
@@ -491,7 +491,7 @@ func (w *WebSSOAuthentication) promptForRole(idp string, roleARNs []string) (rol
 			if err != nil {
 				return "", err
 			}
-			fmt.Fprintln(os.Stderr, rich)
+			w.config.Logger.Warn(rich)
 		}
 		return roleARN, nil
 	}
@@ -552,7 +552,7 @@ func (w *WebSSOAuthentication) promptForIDP(idpARNs []string) (idpARN string, er
 		if err != nil {
 			return "", err
 		}
-		fmt.Fprintln(os.Stderr, rich)
+		w.config.Logger.Warn(rich)
 		return idpARN, nil
 	}
 
@@ -1121,7 +1121,7 @@ func ConsolePrint(config *config.Config, format string, a ...any) {
 		return
 	}
 
-	fmt.Fprintf(os.Stderr, format, a...)
+	config.Logger.Warn(format, a...)
 }
 
 func (w *WebSSOAuthentication) consolePrint(format string, a ...any) {
@@ -1141,7 +1141,7 @@ func (w *WebSSOAuthentication) fetchAllAWSCredentialsWithSAMLRole(idpRolesMap ma
 				defer wg.Done()
 				cc, err := w.awsAssumeRoleWithSAML(iar, assertion, region)
 				if err != nil {
-					fmt.Fprintf(os.Stderr, "failed to fetch AWS creds IdP %q, and Role %q, error:\n%+v\n", iar.idp, iar.role, err)
+					w.config.Logger.Warn("failed to fetch AWS creds IdP %q, and Role %q, error:\n%+v\n", iar.idp, iar.role, err)
 					return
 				}
 				ccch <- cc

cmd/root/debug/debug.go
@@ -17,9 +17,6 @@
 package debug
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
 
 	"github.com/okta/okta-aws-cli/internal/config"
@@ -37,7 +34,7 @@ func NewDebugCommand() *cobra.Command {
 			}
 			err = config.RunConfigChecks()
 			// NOTE: still print out the done message, even if there was an error it will get printed as well
-			fmt.Fprintf(os.Stderr, "debugging okta-aws-cli config $HOME/.okta/okta.yaml is complete\n")
+			config.Logger.Warn("debugging okta-aws-cli config $HOME/.okta/okta.yaml is complete\n")
 			if err != nil {
 				return err
 			}

cmd/root/profileslist/profiles-list.go
@@ -17,8 +17,6 @@
 package profileslist
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
 	"github.com/okta/okta-aws-cli/internal/config"
@@ -35,7 +33,7 @@ func NewProfilesListCommand() *cobra.Command {
 				return err
 			}
 
-			fmt.Println("Profiles:")
+			config.Logger.Info("Profiles:\n")
 
 			keys, err := config.ReadConfigProfileKeys()
 			if err != nil {
@@ -43,7 +41,7 @@ func NewProfilesListCommand() *cobra.Command {
 			}
 
 			for _, key := range keys {
-				fmt.Printf(" %s\n", key)
+				config.Logger.Info(" %s\n", key)
 			}
 
 			return nil

internal/config/config.go
@@ -29,6 +29,8 @@ import (
 
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
+
+	"github.com/okta/okta-aws-cli/internal/logger"
 )
 
 // longUserAgent the long user agent value
@@ -281,6 +283,7 @@ type Config struct {
 	shortUserAgent      bool
 	writeAWSCredentials bool
 	clock               Clock
+	Logger              logger.Logger
 }
 
 // ConfigAttributes attributes for config construction
@@ -324,7 +327,18 @@ func NewEvaluatedConfig() (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	return NewConfig(&cfgAttrs)
+	var config *Config
+	if config, err = NewConfig(&cfgAttrs); err != nil {
+		return nil, err
+	}
+	switch cfgAttrs.Format {
+	case ProcessCredentialsFormat:
+		config.Logger = &logger.TerseLogger{}
+	default:
+		config.Logger = &logger.FullLogger{}
+	}
+
+	return config, nil
 }
 
 // NewConfig create config from attributes
@@ -994,137 +1008,137 @@ awscli:
     "arn:aws:iam::012345678901:role/admin": "Dev Admin"
     "arn:aws:iam::012345678901:role/operator": "Dev Ops"
 	`
-	fmt.Fprintf(os.Stderr, "Given this YAML as an example template of okta.yaml for reference:\n%s\n", exampleYaml)
+	c.Logger.Warn("Given this YAML as an example template of okta.yaml for reference:\n%s\n", exampleYaml)
 
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "WARNING: can't find user home directory $HOME\n")
-		fmt.Fprintf(os.Stderr, "         see https://pkg.go.dev/os#UserHomeDir\n")
+		c.Logger.Warn("WARNING: can't find user home directory $HOME\n")
+		c.Logger.Warn("         see https://pkg.go.dev/os#UserHomeDir\n")
 		return
 	}
-	fmt.Fprintf(os.Stderr, "found home directory %q\n", homeDir)
+	c.Logger.Warn("found home directory %q\n", homeDir)
 
 	configPath := filepath.Join(homeDir, DotOkta, OktaYaml)
 	yamlConfig, err := os.ReadFile(configPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "WARNING: can't read okta config %q\n", configPath)
+		c.Logger.Warn("WARNING: can't read okta config %q\n", configPath)
 		return
 	}
-	fmt.Fprintf(os.Stderr, "okta.yaml is readable %q\n", configPath)
+	c.Logger.Warn("okta.yaml is readable %q\n", configPath)
 
 	conf := map[string]any{}
 	err = yaml.Unmarshal(yamlConfig, &conf)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml is invalid yaml format\n")
+		c.Logger.Warn("WARNING: okta.yaml is invalid yaml format\n")
 		return
 	}
-	fmt.Fprintf(os.Stderr, "okta.yaml is valid yaml\n")
+	c.Logger.Warn("okta.yaml is valid yaml\n")
 
 	awscli, ok := conf["awscli"]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml missing \"awscli\" section\n")
+		c.Logger.Warn("WARNING: okta.yaml missing \"awscli\" section\n")
 		return
 	}
-	fmt.Fprintf(os.Stderr, "okta.yaml has root \"awscli\" section\n")
+	c.Logger.Warn("okta.yaml has root \"awscli\" section\n")
 
 	if awscli == nil {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli\" section has no values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli\" section has no values\n")
 		return
 	}
 	_awscli, ok := awscli.(map[any]any)
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli\" is not a map of values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli\" is not a map of values\n")
 	}
 	idps, ok := _awscli["idps"]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml missing \"awscli.idps\" section\n")
+		c.Logger.Warn("WARNING: okta.yaml missing \"awscli.idps\" section\n")
 		return
 	}
 	if idps == nil {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.idps\" section has no values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.idps\" section has no values\n")
 		return
 	}
 
 	// map[interface {}]interface {}
 	_idps, ok := idps.(map[any]any)
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.idps\" section is not a map of ARN string key to friendly string label values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.idps\" section is not a map of ARN string key to friendly string label values\n")
 		return
 	}
 	if len(_idps) == 0 {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.idps\" section is an empty map of ARN string key to friendly string label values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.idps\" section is an empty map of ARN string key to friendly string label values\n")
 		return
 	}
 
 	for k, v := range _idps {
 		if _, ok := k.(string); !ok {
-			fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.idps\" value of ARN key \"%v\" is not a string\n", k)
+			c.Logger.Warn("okta.yaml \"awscli.idps\" value of ARN key \"%v\" is not a string\n", k)
 			return
 		}
 		if _, ok := v.(string); !ok {
-			fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.idps\" ARN key %q's friendly label value \"%v\" is not a string\n", k, v)
+			c.Logger.Warn("okta.yaml \"awscli.idps\" ARN key %q's friendly label value \"%v\" is not a string\n", k, v)
 			return
 		}
 	}
 
-	fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.idps\" section is a map of %d ARN string keys to friendly string label values\n", len(_idps))
+	c.Logger.Warn("okta.yaml \"awscli.idps\" section is a map of %d ARN string keys to friendly string label values\n", len(_idps))
 
 	roles, ok := _awscli["roles"]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml missing \"awscli.roles\" section\n")
+		c.Logger.Warn("WARNING: okta.yaml missing \"awscli.roles\" section\n")
 		return
 	}
 	if roles == nil {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.roles\" section has no values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.roles\" section has no values\n")
 		return
 	}
 
 	_roles, ok := roles.(map[any]any)
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.roles\" section is not a map of ARN string key to friendly string label values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.roles\" section is not a map of ARN string key to friendly string label values\n")
 		return
 	}
 	if len(_roles) == 0 {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.roles\" section is an empty map of ARN string key to friendly string label values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.roles\" section is an empty map of ARN string key to friendly string label values\n")
 		return
 	}
 
 	for k, v := range _roles {
 		if _, ok := k.(string); !ok {
-			fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.roles\" value of ARN key \"%v\" is not a string\n", k)
+			c.Logger.Warn("okta.yaml \"awscli.roles\" value of ARN key \"%v\" is not a string\n", k)
 			return
 		}
 		if _, ok := v.(string); !ok {
-			fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.roles\" ARN key %q's friendly label value \"%v\" is not a string\n", k, v)
+			c.Logger.Warn("okta.yaml \"awscli.roles\" ARN key %q's friendly label value \"%v\" is not a string\n", k, v)
 			return
 		}
 	}
 
-	fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.roles\" section is a map of %d ARN string keys to friendly string label values\n", len(_roles))
+	c.Logger.Warn("okta.yaml \"awscli.roles\" section is a map of %d ARN string keys to friendly string label values\n", len(_roles))
 
 	profiles, ok := _awscli["profiles"]
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml missing \"awscli.profiles\" section\n")
+		c.Logger.Warn("WARNING: okta.yaml missing \"awscli.profiles\" section\n")
 		return
 	}
 	if profiles == nil {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.profiles\" section has no values\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.profiles\" section has no values\n")
 		return
 	}
 
 	_profiles, ok := profiles.(map[any]any)
 	if !ok {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.profiles\" section is not a map of separate config settings keyed by profile name\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.profiles\" section is not a map of separate config settings keyed by profile name\n")
 		return
 	}
 	if len(_profiles) == 0 {
-		fmt.Fprintf(os.Stderr, "WARNING: okta.yaml \"awscli.profiles\" section is an empty map of separate config settings keyed by profile name\n")
+		c.Logger.Warn("WARNING: okta.yaml \"awscli.profiles\" section is an empty map of separate config settings keyed by profile name\n")
 		return
 	}
 
-	fmt.Fprintf(os.Stderr, "okta.yaml \"awscli.profiles\" section is a map of %d separate config settings keyed by profile name\n", len(_profiles))
+	c.Logger.Warn("okta.yaml \"awscli.profiles\" section is a map of %d separate config settings keyed by profile name\n", len(_profiles))
 
-	fmt.Fprintf(os.Stderr, "okta.yaml is OK\n")
+	c.Logger.Warn("okta.yaml is OK\n")
 	return nil
 }
 

internal/exec/exec.go
@@ -23,17 +23,19 @@ import (
 	"strings"
 
 	oaws "github.com/okta/okta-aws-cli/internal/aws"
+	"github.com/okta/okta-aws-cli/internal/config"
 	"github.com/okta/okta-aws-cli/internal/utils"
 )
 
 // Exec is a executor / a process runner
 type Exec struct {
-	name string
-	args []string
+	name   string
+	args   []string
+	config *config.Config
 }
 
 // NewExec Create a new executor
-func NewExec() (*Exec, error) {
+func NewExec(c *config.Config) (*Exec, error) {
 	args := []string{}
 	foundArgs := false
 	for _, arg := range os.Args {
@@ -55,8 +57,9 @@ func NewExec() (*Exec, error) {
 	name := args[0]
 	args = args[1:]
 	ex := &Exec{
-		name: name,
-		args: args,
+		name:   name,
+		args:   args,
+		config: c,
 	}
 
 	return ex, nil
@@ -85,14 +88,14 @@ func (e *Exec) Run(cc *oaws.CredentialContainer) error {
 
 	out, err := cmd.Output()
 	if ee, ok := err.(*osexec.ExitError); ok {
-		fmt.Fprintf(os.Stderr, "error running process\n")
-		fmt.Fprintf(os.Stderr, "%s %s\n", e.name, strings.Join(e.args, " "))
-		fmt.Fprintf(os.Stderr, utils.PassThroughStringNewLineFMT, ee.Stderr)
+		e.config.Logger.Warn("error running process\n")
+		e.config.Logger.Warn("%s %s\n", e.name, strings.Join(e.args, " "))
+		e.config.Logger.Warn(utils.PassThroughStringNewLineFMT, ee.Stderr)
 	}
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%s", string(out))
+	e.config.Logger.Info("%s", string(out))
 	return nil
 }

internal/logger/full.go
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2025-Present, Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+// FullLogger logger for stderr (warn) and stdout (info) logging
+type FullLogger struct {
+}
+
+func (l *FullLogger) Info(format string, a ...any) (int, error) {
+	return fmt.Fprintf(os.Stdout, format, a...)
+}
+
+func (l *FullLogger) Warn(format string, a ...any) (int, error) {
+	return fmt.Fprintf(os.Stderr, format, a...)
+}

internal/logger/logger.go
@@ -1,0 +1,23 @@
+/*
+ * Copyright (c) 2025-Present, Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package logger
+
+// Logger Interface used for logging ouput
+type Logger interface {
+	Info(format string, a ...any) (int, error)
+	Warn(format string, a ...any) (int, error)
+}

internal/logger/terse.go
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2025-Present, Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package logger
+
+import (
+	"fmt"
+	"os"
+)
+
+// TerseLogger logger that only prints info level lines
+type TerseLogger struct {
+}
+
+func (l *TerseLogger) Info(format string, a ...any) (int, error) {
+	return fmt.Fprintf(os.Stdout, format, a...)
+}
+
+func (l *TerseLogger) Warn(format string, a ...any) (int, error) {
+	return -1, nil
+}

commit 73340e687d8c8c6e1ad90429c27d95370bd2dc96

Fix output formatting bug introduced in 9f8a7ec

internal/webssoauth/webssoauth.go
@@ -275,7 +275,7 @@ func (w *WebSSOAuthentication) selectFedApp(apps []*okta.Application) (string, e
 				if err != nil {
 					return "", err
 				}
-				w.config.Logger.Warn(rich)
+				w.config.Logger.Warn(rich + "\n")
 			}
 
 			return app.ID, nil
@@ -491,7 +491,7 @@ func (w *WebSSOAuthentication) promptForRole(idp string, roleARNs []string) (rol
 			if err != nil {
 				return "", err
 			}
-			w.config.Logger.Warn(rich)
+			w.config.Logger.Warn(rich + "\n")
 		}
 		return roleARN, nil
 	}
@@ -552,7 +552,7 @@ func (w *WebSSOAuthentication) promptForIDP(idpARNs []string) (idpARN string, er
 		if err != nil {
 			return "", err
 		}
-		w.config.Logger.Warn(rich)
+		w.config.Logger.Warn(rich + "\n")
 		return idpARN, nil
 	}
 